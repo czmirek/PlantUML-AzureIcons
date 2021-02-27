5f92e15615dfacb60aac5a90bec7e74e2434295c
Miroslav Bartl <18615545+czmirek@users.noreply.github.com>
2021-02-27 16:28:59 +0100

    Build run with inkscape, rsvg-convert is producing broken images...

--- build.ps1
+++ build.ps1
@@ -7,19 +7,11 @@ $iconFolder = "icon-collection"
 $iconPath = "$($iconFolder)/azure-cds/*.svg"
 $dist = "dist"
 
-try {
-    Remove-Item $dist -Recurse -Force -Confirm:$false
-    New-Item $dist -ItemType "directory"
-} catch {}
-
-
 if(!(Test-Path $iconFolder)) {
     git clone $sourceCollection $iconFolder
 }
 
-Get-ChildItem $iconPath | ForEach-Object -Parallel {
-    $dist = $using:dist
-    $targetHeight = $using:targetHeight
+Get-ChildItem $iconPath | ForEach-Object {
     
     $fileName = $_
     $fullPath = $_.FullName
@@ -36,35 +28,18 @@ Get-ChildItem $iconPath | ForEach-Object -Parallel {
     
     Write-Host "Processing $($serviceId)"
 
-    $svgOutput = $dist + "/$($serviceId).svg"
     $pngOutput = $dist + "/$($serviceId).png"
     $pumlOutput = $dist + "/$($serviceId).puml"
     
-    rsvg-convert --height $targetHeight `
-                 --width $targetHeight `
-                 --keep-aspect-ratio `
-                 --output "$($svgOutput)" `
-                 --format svg `
-                 --background-color none `
-                 "$($fullPath)"
+    inkscape --export-type="png" -w $targetHeight -h $targetHeight "$($fullPath)" -o $pngOutput
 
-    rsvg-convert --height $targetHeight `
-                 --width $targetHeight `
-                 --keep-aspect-ratio `
-                 --output "$($pngOutput)" `
-                 --format png `
-                 --background-color white `
-                 "$($fullPath)"
-
-    $sprite = ((java -jar "$($using:plantUmlPath)" -encodesprite "16z" "$($pngOutput)") | Out-String) -replace "`r", ""
+    $sprite = ((java -jar "$($plantUmlPath)" -encodesprite "16z" "$($pngOutput)") | Out-String) -replace "`r", ""
     $puml = $sprite
     $puml += "AzureEntityColoring($($serviceId))`n"
     $puml += "!define $($serviceId)(e_alias, e_label, e_techn) AzureEntity(e_alias, e_label, e_techn, AZURE_SYMBOL_COLOR, $($serviceId), $($serviceId))`n"
     $puml += "!define $($serviceId)(e_alias, e_label, e_techn, e_descr) AzureEntity(e_alias, e_label, e_techn, e_descr, AZURE_SYMBOL_COLOR, $($serviceId), $($serviceId))`n"
     $puml | Out-File $pumlOutput -NoNewLine
-
-} -ThrottleLimit 16
-
+}
 
 $allPuml = ""
 Get-ChildItem ($dist + "/*.puml") | ForEach-Object { 
@@ -91,4 +66,4 @@ Get-ChildItem ($dist + "/*.puml") | ForEach-Object {
     $serviceId = [System.IO.Path]::GetFileNameWithoutExtension($fileName)
     $markdownList +="$($serviceId) | ![$($serviceId)](dist/$($serviceId).png) |`n"
 }
-$markdownList | Out-File "dist/table.md" -NoNewLine
+$markdownList | Out-File "table.md" -NoNewLine
